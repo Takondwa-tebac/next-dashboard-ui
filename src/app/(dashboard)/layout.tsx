e055670e6894013193baa0f887bc077fa3741e4f
Takondwa-tebac <takondwakapyolah@gmail.com>
2024-10-24 10:40:26 +0200

    updated sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -6,13 +8,12 @@ export default function DashboardLayout({
   return (
    
       <div className={"flex  gap-2 min-h[100vh]"}>
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-600">Side BR</div>
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] min-h-[100vh]">
+          <Menu />
+        </div>
         <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-white/30">
         {children}
         </div>
-
-       
-        
      </div>
    
   );
