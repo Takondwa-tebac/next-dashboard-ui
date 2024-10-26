50b7d8434bd78a3ecf37b34d16bbf94c19e6c9f4
Takondwa-tebac <takondwakapyolah@gmail.com>
2024-10-26 23:38:32 +0200

    updated parent page

---
+++ src/app/(dashboard)/parent/index.jsx
@@ -0,0 +1,15 @@
+const ParentPage = () => {
+    return (
+        <div className="p-5 felx flex-col md:fle-row">
+        {/* left */}
+         <div className="2/3">
+         
+         </div>
+          {/* right */}
+         <div className="1/3">
+         </div>
+      </div>
+    )
+}
+
+export default ParentPage
