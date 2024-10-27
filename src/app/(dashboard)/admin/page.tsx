be6650095d4c67250564c205395bb8d9d0795f80
Takondwa-tebac <takondwakapyolah@gmail.com>
2024-10-27 09:41:26 +0200

    updated admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,21 +1,40 @@
 import StatCard from '@/components/stats-card';
+import CountChart from '@/components/countchart';
+
 
 const AdminPage = () => {
     return (
-        <div className="p-5 felx flex-col md:fle-row">
+        <div className="p-5 flex flex-col md:flex-row">
             {/* left */}
             <div className="2/3">
-
+                    {/* cards */}
                 <div className="flex gap-4 flex-wrap">
                     <StatCard  type={{user:"Students",count:6123}}/>
                     <StatCard  type={{user:"Parents",count:1123}}/>
                     <StatCard  type={{user:"Staffs",count:34}}/>
                     <StatCard  type={{user:"Teachers",count:20}}/>
                 </div>
+
+                {/* charts   */}
+
+                <div className="flex flex-col md:flex-row justify-between">
+                    <div className="w-full md:w-1/2">
+                        <CountChart />
+                    </div>
+
+                    <div className="w-full w-1/2">
+                        <span> other stats </span>
+                    </div>
+
+                </div>
             </div>
+
+
             {/* right */}
             <div className="1/3">
             </div>
+
+            
         </div>
     )
 }
--- src/components/stats-card.jsx
+++ src/components/stats-card.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link"
 export default function StatCard({type}){
     return (
-        <div className="rounded-2xl even:bg-purple-600  p-4 odd:bg-yellow-200">
+        <div className="rounded-2xl even:bg-purple-300  p-4 odd:bg-yellow-100">
                 <div className="flex gap-3  justify-between items-center">
                     <span className="rounded-lg bg-white p-1 text-xs">{new Date().getFullYear() }</span>
                     <Link href={"#"}>
-                        <img src="/more.png" alt="more" />
+                        <img src="/more.png" className="w-4 h-2" alt="more" />
                     </Link>
                 </div>
 
