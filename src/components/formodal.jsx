0d616c0bb608e129c7700d4ec6c9e7b6f202f947
Takondwa-tebac <takondwakapyolah@gmail.com>
2024-11-03 04:35:20 +0200

    updated teacher form

--- src/components/formodal.jsx
+++ src/components/formodal.jsx
@@ -21,19 +21,33 @@ export default function FormModal({ table, type, id, data }) {
         <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
           <div className="bg-white rounded-md p-4 relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
             <button
-              className="w-6 h-6 bg-gray-200 rounded-full absolute top-4 right-4 flex items-center justify-center cursor-pointer"
+              className="w-6 h-6 bg-gray-200 rounded-full absolute top-5 right-6 flex items-center justify-center cursor-pointer"
               onClick={() => setOpen(false)}
             >
               <span className="text-black text-lg font-bold">X</span>
             </button>
 
-            <div className="my-2 bg-gray-100  w-full p-4 rounded-md">
-              <span className="text-xl font-bold text-white">
+            {/* <div className="my-2 bg-gray-300  w-full p-4 rounded-md"> */}
+              <span className="text-xl font-bold text-gray-600">
                 {type} {table}
               </span>
-            </div>
+            {/* </div> */}
 
-            {/* Add your form or other content here */}
+            <div className="my-4">
+                <form action="#" method="Post">
+                    <div className="form-control">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name"  className="input" placeholder="Enter Name" />
+                    </div>
+
+
+                    <div className="form-control w-full">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" className="input" placeholder="Enter Name" />
+                    </div>
+
+                </form>
+            </div>
           </div>
         </div>
       )}
