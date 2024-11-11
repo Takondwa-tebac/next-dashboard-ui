82f0296c4f792ce3831f76da92cb74ab6a109d39
Takondwa-tebac <takondwakapyolah@gmail.com>
2024-11-11 03:24:02 +0200

    updated setup

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "@/styles/globals.css";
+import {SnackbarProvider} from "notistack";
+
+export default function App({Component, pageProps}) {
+    return (<SnackbarProvider maxSnack={3}>
+        <Component {...pageProps} />
+    </SnackbarProvider>)
+}
+
